8661c4a5af03fcdf70d8dfb4b1aa5aeeb0e2e0e6
Ramsey Nasser <ram@nas.sr>
2014-10-31 01:06:54 -0400

    Add flocking

---
+++ Arcadia/Scripts/catcon/cat.clj
@@ -0,0 +1,94 @@
+(ns catcon.cat
+  (:use arcadia.core
+        catcon.linear)
+  (:import [UnityEngine Time Vector3 Mathf Physics Gizmos Color Transform]))
+
+(defn random-vector ^Vector3 []
+  (v* (UnityEngine.Random/onUnitSphere) 
+      (Vector3. 1 0 1)))
+
+(declare cat-update)
+
+(defcomponent Cat [^Vector3 heading
+                   ^float rotate-speed]
+  (Start [this]
+    (set! rotate-speed 1.0)
+    (set! (.. this heading) (random-vector)))
+  
+  (Update [this]
+    (catcon.cat/cat-update this))
+  (OnDrawGizmos [this]
+    (set! Gizmos/color Color/green)
+    (Gizmos/DrawRay (.. this transform position)
+                    (.. this heading))
+    (set! Gizmos/color Color/red)
+    (Gizmos/DrawRay (.. this transform position)
+                    (.. this transform forward))))
+
+(defn flockmates [^Vector3 pos ^double radius]
+  (->> (Physics/OverlapSphere pos radius Physics/AllLayers)
+       (filter #(= (.tag %) "Cat"))))
+
+(defn near [^Vector3 pos ^double radius ^System.Type component]
+  (->> (flockmates pos radius)
+       (map #(get-component % component))))
+
+(defn near-headings [^Vector3 pos ^double radius]
+  (->> (near pos radius catcon.cat.Cat)
+       (map #(.heading %))))
+
+(defn v÷ ^Vector3 [^Vector3 v1 ^Vector3 v2]
+  (Vector3/op_Division v1 v2))
+
+(defn alignment [h headings speed]
+  (let [average (v÷ (reduce v+ headings) (count headings))]
+    (Vector3/RotateTowards h average speed 0)))
+
+(defn cohesion [h p positions speed]
+  (let [average (v÷ (reduce v+ positions) (count positions))]
+    (Vector3/RotateTowards h (v- average p) speed 0)))
+
+(defn separation [h p positions speed]
+  (let [fs (map #(v- p %) positions)
+        f (reduce v+ fs)]
+    (Vector3/RotateTowards h f speed 0)))
+
+(defn cat-update [^Cat this]
+  (comment ;; wandering
+    (set! (.. this heading) (random-vector)))
+  
+  (let [position (.. this transform position)
+        
+        flock-big (flockmates position 6)
+        flock-med (flockmates position 4)
+        flock-sml (flockmates position 2)
+        
+        positions-big (map #(.. % transform position)
+                       flock-big)
+        positions-sml (map #(.. % transform position)
+                       flock-sml)
+        headings (map #(.. % (GetComponent Cat) heading)
+                      flock-med)
+        
+        speed (* Time/deltaTime (.. this rotate-speed))
+        heading (-> (.. this heading)
+                    (cohesion position positions-big 2)
+                    (separation position positions-sml 60)
+                    (alignment headings 2))]
+    (set! (.. this transform forward)
+          (Vector3/RotateTowards (.. this transform forward)
+                                 heading speed 0))))
+
+(comment
+  (doseq [x (range 0 30 2)
+          y (range 0 30 2)]
+  (instantiate (object-named "/Cat") (Vector3. x 0 y)))
+
+(dorun (map destroy (objects-named "Cat(Clone)")))
+
+(set! (.angle (get-component (object-named "/Cat") catcon.cat.Cat)) 360)
+
+(.GetInstanceID (object-named "/Cat"))
+
+(set! (.. (object-named "/Cat") transform forward) Vector3/forward))
+
