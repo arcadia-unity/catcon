dad63481049410a890202b4cd149bd57abc93012
Ramsey Nasser <ram@nas.sr>
2014-11-03 15:38:05 -0500

    Add camera component

---
+++ Arcadia/Scripts/catcon/camera.clj
@@ -0,0 +1,15 @@
+﻿(ns catcon.camera
+  (:use arcadia.core
+        catcon.linear)
+  (:import [UnityEngine Debug Input Vector3 Space]))
+
+(defcomponent Camera [^Vector3 target
+                      ^Vector3 focus]
+  (Update [this]
+    (let [towards-target (v* 0.1 (v- target focus))]
+      (set! (.focus this) (v+ focus towards-target))
+      (doto (.transform this)
+        (.Translate towards-target Space/World)
+        (.RotateAround focus Vector3/up (Input/GetAxis "Horizontal"))
+        (.Translate 0 (Input/GetAxis "Vertical") 0)
+        (.LookAt focus)))))
