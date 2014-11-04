c52740a30b361cfe3b108ce3e3b8859d80a4d174
Ramsey Nasser <ram@nas.sr>
2014-11-03 19:35:20 -0500

    Add adjustable camera speed

--- Arcadia/Scripts/catcon/camera.clj
+++ Arcadia/Scripts/catcon/camera.clj
@@ -3,13 +3,14 @@
         catcon.linear)
   (:import [UnityEngine Debug Input Vector3 Space]))
 
-(defcomponent Camera [^Vector3 target
+(defcomponent Camera [^float speed
+                      ^Vector3 target
                       ^Vector3 focus]
   (Update [this]
     (let [towards-target (v* 0.1 (v- target focus))]
       (set! (.focus this) (v+ focus towards-target))
       (doto (.transform this)
         (.Translate towards-target Space/World)
-        (.RotateAround focus Vector3/up (Input/GetAxis "Horizontal"))
-        (.Translate 0 (Input/GetAxis "Vertical") 0)
+        (.RotateAround focus Vector3/up (* speed (Input/GetAxis "Horizontal")))
+        (.Translate 0 (* speed (Input/GetAxis "Vertical")) 0)
         (.LookAt focus)))))
